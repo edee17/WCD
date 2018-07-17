88d08af289613bb4e1657b949aa3d61eef57a673
edee17 <muhammadmuqtasidin17.18@ichat.sp.edu.sg>
2018-07-17 22:07:14 +0800

    UnitM

---
+++ UnitM/script.js
@@ -0,0 +1,40 @@
+/* 
+   Revisions Bookstore and Cafe 
+   Filename: script.js
+
+   Author: Muhd Muqtasidin Bin Ahmad Eunos   
+   Date: 17 July 2018     
+   HTML5 and CSS3 Illustrated Unit M, Visual Workshop
+ */
+
+var smallsize = document.querySelector(".smaller-size");
+var defaultsize = document.querySelector(".selected-button");
+var largesize = document.querySelector(".larger-size");
+var text = document.querySelector("html");
+var font = document.querySelector("fontsize");
+
+function smaller(){
+  smallsize.className = "smaller-size, selected-button";
+  text.className = "html, smaller-size";
+  defaultsize.className = "selected-button, html";
+  largesize.className = "larger-size, larger-size";
+}
+
+function normal(){
+  defaultsize.className = "selected-button, selected-button";
+  text.className = "html, selected-button";
+  smallsize.className = "smaller-size, smaller-size";
+  largesize.className = "larger-size, larger-size";
+}
+
+function larger(){
+  largesize.className = "larger-size, selected-button";
+  text.className = "html, larger-size"
+  defaultsize.className = "selected-button, html";
+  smallsize.className = "smaller-size, smaller-size";
+}
+
+smallsize.addEventListener("click", smaller,);
+defaultsize.addEventListener("click", normal);
+largesize.addEventListener("click", larger);
+
