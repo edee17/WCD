6c2df5a8037af4fb0c9dd6660db32c31153d271e
edee17 <muhammadmuqtasidin17.18@ichat.sp.edu.sg>
2018-08-12 18:01:37 +0800

    CA2

---
+++ CA2/loader.js
@@ -0,0 +1,14 @@
+var myVar;
+
+function loaderFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function loaderFunction2() {
+    myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
